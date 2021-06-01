1894b4268c09e0510a819fd9a8846d35fe526c71
yuya-taniguti <yuya2624@gmail.com>
2021-06-01 19:22:13 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :room_users
+  #has_many :rooms, through: room_users
+  has_many :messages 
+  has_many :Photos
+
+  with_options presence: true do
+    validates :name
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  end
 end
--- db/migrate/20210601053827_devise_create_users.rb
+++ db/migrate/20210601053827_devise_create_users.rb
@@ -4,8 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,                null: false, default: ""
+      t.string :encrypted_password,   null: false, default: ""
+      t.string :name,                 null: false
+      t.string :nickname,             null: false
 
       ## Recoverable
       t.string   :reset_password_token
